e0328e24b46773249d7403c1348d62d913f6a21a
NotFearless <46384037+NotFearless@users.noreply.github.com>
2025-06-11 01:33:26 -0500

    Fix Tailwind CSS config and styling

--- nailwind.config.js
+++ nailwind.config.js
@@ -5,11 +5,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      animation: {
-        'bounce-slow': 'bounce 1s infinite',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
